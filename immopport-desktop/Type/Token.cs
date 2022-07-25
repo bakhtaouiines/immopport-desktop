e1116b95dfaafa9529d83676fa79f5e1deb562ef
bakhtaouiines <inesbkht@gmail.com>
2022-07-25 16:58:36 +0200

    create type folder + type Token file

---
+++ immopport-desktop/Type/Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace immopport_desktop.Type
+{
+    public class Token
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; } = string.Empty;
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = string.Empty;
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+    }
+}
