24bad9c19ed0bc89fe7d605cf3855d89c1b6cbdc
bakhtaouiines <inesbkht@gmail.com>
2022-07-25 13:14:49 +0200

    rename MainWindow to Login

---
+++ immopport-desktop/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace immopport_desktop
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (txtMatricule.Text.Length == 0)
+            {
+                errormessage.Text = "Veuillez renseigner votre matricule.";
+                txtMatricule.Focus();
+            }
+        }
+    }
+}
