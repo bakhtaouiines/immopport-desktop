cde7ae8d37ec0cbd1e3d5956b95117f79c18307d
bakhtaouiines <inesbkht@gmail.com>
2022-07-26 14:57:30 +0200

    refactorization API class

--- immopport-desktop/API.cs
+++ immopport-desktop/API.cs
@@ -10,30 +10,68 @@ namespace immopport_desktop
 {
     public class API 
     {
-        public async Task Auth()
-        {
-            string baseURL = "http://api.immopport.cda.ve.manusien-ecolelamanu.fr/api/public/";
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(baseURL);
+        static readonly string baseURL = "http://api.immopport.cda.ve.manusien-ecolelamanu.fr/api/public/";
+        static readonly HttpClient client = new HttpClient();
 
+        public object? Token //property
+        {
+            get;
+            set;
+        }
+
+        //Constructeur de la classe
+        public  API() {
+            client.BaseAddress = new Uri(baseURL);
+            // add content type
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            // add UTF-8
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("UTF8"));
+        }
+
+        private  async Task<T?> GetApi<T>(string URI)
+        {
             try
             {
-                // add content type
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                // add UTF-8
-                client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("UTF8"));
                 // fetch JTW token
-                HttpResponseMessage response = await client.GetAsync(client.BaseAddress + "/authentification/employee?matricule=14747&password=Password14!");
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress + URI);
                 // save the token for further requests.
-                Token? token = JsonSerializer.Deserialize<Token>(response.Content.ReadAsStream());
+                T? token = JsonSerializer.Deserialize<T>(response.Content.ReadAsStream());
+                Application.Current.Properties["access_token"] = token;
 
-                if (token != null)
-                {                  
+                return token;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return default(T);
+            }
+        }
+
+        public async Task<Token?> Auth()
+        {
+            try
+            {
+                return await GetApi<Token?>("/authentification/employee?matricule=14747&password=Password14!");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return default(Token);
+            }
+        }
+
+        public async Task<EmployeeResponse?> GetProfile()
+        {
+            try
+            {
+                if (Auth() != null)
+                {
+                    MessageBox.Show(TokenType);
                     // Set the authentication header. 
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(token.TokenType, token.AccessToken);
-                    
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(TokenType, AccessToken);
+
                     // get employee informations
-                    HttpResponseMessage employees = await client.GetAsync(client.BaseAddress + "/employee/dashboard");
+                    HttpResponseMessage employees = await GetApi<EmployeeResponse?>("/employee/dashboard");
 
                     EmployeeResponse? responseBody = JsonSerializer.Deserialize<EmployeeResponse>(employees.Content.ReadAsStream());
                     if (responseBody != null && responseBody.Employee != null)
@@ -53,7 +91,27 @@ namespace immopport_desktop
             catch ( Exception e)
             {
                 MessageBox.Show(e.Message);
+                return default(EmployeeResponse);
             }
         }
+
+        /*public async Task GetAllEmployees()
+        {
+            client.BaseAddress = new Uri(baseURL);
+
+            try
+            {
+                // add content type
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                // add UTF-8
+                client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("UTF8"));
+
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress + "/employee/");
+
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }*/
     }
 }
