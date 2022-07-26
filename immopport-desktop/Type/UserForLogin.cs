0ed952c0b7409c9d06d2de31ecdc5d859a965ce0
bakhtaouiines <inesbkht@gmail.com>
2022-07-26 15:24:35 +0200

    update on passwordbox

---
+++ immopport-desktop/Type/UserForLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace immopport_desktop.Type
+{
+    public class UserForLogin
+    {
+        public int Matricule { get; set; }
+        public string Password { get; set; } = string.Empty;
+    }
+}
