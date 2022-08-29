1ec1306cefb1a9db46b04238ddbc09ee8a2a3f6c
Taslima-Ahamed-Mze <taslimaahamedmze@gmail.com>
2022-08-29 12:22:17 +0200

    Create Type property

---
+++ immopport-desktop/Type/Property.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace immopport_desktop.Type
+{
+    public class Property
+    {
+
+        [JsonPropertyName("id")]
+        public int PropertyId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Titre { get; set; } = string.Empty;
+
+        [JsonPropertyName("address")]
+        public string? Adresse { get; set; } = string.Empty;
+
+        [JsonPropertyName("price")]
+        public int Prix { get; set; }
+    }
+
+    public class PropertyResponse
+    {
+        [JsonPropertyName("property")]
+        public Property? Property { get; set; }
+    }
+}
