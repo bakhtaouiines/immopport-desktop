f4d1287261bd993910569dd7d5e736590164be89
bakhtaouiines <inesbkht@gmail.com>
2022-10-03 13:26:54 +0200

    new file for display and logic of single property fetch

---
+++ immopport-desktop/SingleProperty.xaml.cs
@@ -0,0 +1,48 @@
+﻿using immopport_desktop.Type;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace immopport_desktop
+{
+    public partial class SingleProperty : UserControl
+    {
+        public SingleProperty(string propertyId)
+        {
+            InitializeComponent();
+
+            API user;
+
+            if (Application.Current != null && Application.Current.Properties["user"] != null)
+            {
+                user = (API)Application.Current.Properties["user"];
+                Task<PropertyResponse?>? property = Task.Run(() => user?.GetSingleProperty(propertyId));
+
+                if (property != null)
+                {
+                    MessageBox.Show(property.Result.Property.Address);
+
+                    List<Property> items = new List<Property>((IEnumerable<Property>)property.Result.Property);
+                    PropertyDisplay.ItemsSource = items;
+                }
+                else
+                {
+                    MessageBox.Show(user?.ErrorMessage);
+                }
+
+            }
+        }
+    }
+}
